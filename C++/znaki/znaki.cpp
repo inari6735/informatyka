2b7d0611e8bbdc13fe60da6e30b0f9d8796d134f
inari6735 <piotrek1723@gmail.com>
2018-11-07 10:35:54 +0100

    kod ascii

--- C++/znaki/znaki.cpp
+++ C++/znaki/znaki.cpp
@@ -9,15 +9,41 @@
 
 using namespace std;
 
+void zamien_znaki(char tb[], int roz){
+    for(int i = 0; i < roz; i++){
+        zamień małe na duże;
+        kod_ascii 'a' = 97
+        kod_ascii 'A' = 65
+        97 - 65 = 32
+        (char)(kod_ascii - 32)
+    }
+}
+
 void licz_znaki(char tb[], int roz){
     //for(int i = 0; i < roz; i++){
         //cout << tb[i];
     //}
     int i = 0;
+    int biale, cyfry, literyD, literyM, reszta;
+    biale = cyfry = literyD = literyM = reszta = 0;
+    int znak_kod = 0;
     while(tb[i] != '\0'){
-        cout << tb[i];
+        znak_kod = (int)tb[i];
+        if(znak_kod > 64 && znak_kod < 91)
+            literyD++;
+        else if(znak_kod > 96 && znak_kod < 123)
+            literyM++;
+        else if(znak_kod > 47 && znak_kod < 58)
+            cyfry++;
+        else
+            reszta++;
         i++;
-    }
+        }
+    cout << "Białych: " << biale << endl;
+    cout << "Duże: " << literyD << endl;
+    cout << "Małe: " << literyM << endl;
+    cout << "Cyfry: " << cyfry << endl;
+    cout << "Reszta: " << reszta << endl;
 }
 
 int main(int argc, char **argv)
