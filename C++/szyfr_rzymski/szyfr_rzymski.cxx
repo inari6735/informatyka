00814390a1cf884b9a09922f8a47d2ca582456b9
inari6735 <piotrek1723@gmail.com>
2019-02-20 10:32:59 +0100

    Orki z Majorki do Stalowej Woli

--- C++/szyfr_rzymski/szyfr_rzymski.cxx
+++ C++/szyfr_rzymski/szyfr_rzymski.cxx
@@ -9,27 +9,48 @@ using namespace std;
 
 #define MAKS 100
 
-void szyfruj(char tekst[], int klucz){
-    int tekstSize = strlen(tekst);
+void deszyfruj(char tb[], int klucz){
+    klucz = klucz % 26;
+    int i = 0;
     int kod = 0;
-    for(int i=0; i < tekstSize; i++){
-        cout << tekst[i] << endl;
-    };
-    for(int j=0; i < tekstSize; i++){
-        kod = (int)tekst[i];
-        if kod
+    while (tb[i] != '\0'){
+        ;
     }
+}
+
+void szyfruj(char tb[], int klucz){
+    klucz = klucz % 26;
+    int i = 0;
+    int kod = 0;
+    while (tb[i] != '\0'){
+        kod = (int)tb[i];
+        if(tb[i] == ' '){
+            ;
+        }else if(kod < 91){
+            kod += klucz;
+            if(kod > 90) kod -= 26;
+        }else{
+            kod += klucz;
+            if(kod > 122) kod -= 26;
+        }
+        
+        
+        cout << (char)kod;
+        tb[i] = (char)kod;
+        i++;
+    }
+    cout << endl;
 }
 
 int main(int argc, char **argv)
 {
-    char tekst[MAKS];
+    char tb[MAKS];
     int klucz = 0;
     cout << "Podaj tekst:\n";
-    cin.getline(tekst, MAKS);
+    cin.getline(tb, MAKS);
     cout << "Podaj klucz: ";
     cin >> klucz;
-    szyfruj(tekst, klucz);
+    szyfruj(tb, klucz);
     
 	return 0;
 }
