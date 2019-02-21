e38a52271c8123f51da4a530cddc7bf8d699aa27
inari6735 <piotrek1723@gmail.com>
2019-02-21 07:52:07 +0100

    Zabaw

--- C++/szyfr_rzymski/szyfr_rzymski.cxx
+++ C++/szyfr_rzymski/szyfr_rzymski.cxx
@@ -14,8 +14,23 @@ void deszyfruj(char tb[], int klucz){
     int i = 0;
     int kod = 0;
     while (tb[i] != '\0'){
-        ;
+        kod = (int)tb[i];
+        if(tb[i] == ' '){
+            ;
+        }else if(kod < 91){
+            kod -= klucz;
+            if(kod < 90) kod -= 26;
+        }else{
+            kod += klucz;
+            if(kod > 122) kod -= 26;
+        }
+        
+        
+        cout << (char)kod;
+        tb[i] = (char)kod;
+        i++;
     }
+    cout << endl;
 }
 
 void szyfruj(char tb[], int klucz){
@@ -51,6 +66,7 @@ int main(int argc, char **argv)
     cout << "Podaj klucz: ";
     cin >> klucz;
     szyfruj(tb, klucz);
+    deszyfruj(tb, klucz);
     
 	return 0;
 }
